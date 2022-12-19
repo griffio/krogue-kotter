3a9da2b824317ee7046ce430be28331b87495fe0
griffio <griffio@users.noreply.github.com>
2022-12-19 10:47:22 +0000

    Add status panels and resize

--- src/main/kotlin/griffio/krogue/Main.kt
+++ src/main/kotlin/griffio/krogue/Main.kt
@@ -14,22 +14,27 @@ import com.varabyte.kotter.terminal.virtual.TerminalSize
 import com.varabyte.kotter.terminal.virtual.VirtualTerminal
 import com.varabyte.kotterx.decorations.BorderCharacters
 import com.varabyte.kotterx.decorations.bordered
-import com.varabyte.kotterx.text.shiftRight
+import griffio.krogue.rooms.TOTAL_CASH
 import griffio.krogue.rooms.generateRooms
 import java.time.Duration
 
-val world: List<MutableList<Tile>> = generateRooms().map { line ->
-    line.map {
-        when (it) {
-            '#' -> Cave()
-            '.' -> Floor()
-            '^' -> Lava()
-            '~' -> Water()
-            '@' -> Hero
-            else -> error("Unknown tile: $it")
-        }
-    }.toMutableList()
+fun generateWorld(): List<MutableList<Tile>> {
+    return generateRooms().map { line ->
+        line.map {
+            when (it) {
+                '#' -> Cave()
+                '.' -> Floor()
+                '^' -> Lava()
+                '~' -> Water()
+                '£' -> Cash()
+                '@' -> Hero
+                else -> error("Unknown tile: $it")
+            }
+        }.toMutableList()
+    }
 }
+
+val world: List<MutableList<Tile>> = generateWorld()
 
 data class View(val min: Int, val max: Int)
 
@@ -42,13 +47,21 @@ fun decView(current: View, dec: Int, min: Int) =
     if (current.min > min) current.min - dec xy current.max - dec else current
 
 // https://en.wikipedia.org/wiki/ANSI_escape_code (Standard colors/High-intensity colors)
-sealed class Tile(val glyph: Char, val colorIndex: Int, var isVisible: Boolean, var isOpaque: Boolean)
-class Floor() : Tile('.', 15, false, false)
-class Cave() : Tile('#', 7, false, true)
-class Lava() : Tile('^', 9, false, false)
-class Water() : Tile('~', 14, false, false)
-object Empty : Tile(' ', 0, false, false)
-object Hero : Tile('@', 11, false, false)
+sealed class Tile(
+    val glyph: Char,
+    val colorIndex: Int,
+    var isVisible: Boolean,
+    var isOpaque: Boolean,
+    var isTaken: Boolean,
+)
+
+class Floor() : Tile('.', 15, false, false, false)
+class Cave() : Tile('#', 7, false, true, false)
+class Lava() : Tile('^', 9, false, false, false)
+class Water() : Tile('~', 14, false, false, false)
+class Cash() : Tile('£', 11, false, false, false)
+object Empty : Tile(' ', 0, false, false, false)
+object Hero : Tile('@', 11, false, false, false)
 
 fun RenderScope.render(t: Tile, isInverted: Boolean) {
     if (isInverted) invert()
@@ -60,10 +73,9 @@ val HEIGHT = world.size
 val WIDTH = world[0].size
 
 fun main() = session(
-
     terminal = listOf(
         { SystemTerminal() },
-        { VirtualTerminal.create(terminalSize = TerminalSize(WIDTH, HEIGHT)) }
+        { VirtualTerminal.create(terminalSize = TerminalSize(WIDTH - (WIDTH / 3), HEIGHT - (HEIGHT / 3))) }
     ).firstSuccess(),
     clearTerminal = true,
 ) {
@@ -73,18 +85,35 @@ fun main() = session(
     var yView by liveVarOf(0 xy yMaxIndex / 2)
     var xhero by liveVarOf(xMaxIndex / 4)
     var yhero by liveVarOf(yMaxIndex / 4)
-    val padRight = (WIDTH - WIDTH / 2) / 2
-    val padTop = (HEIGHT - HEIGHT / 2) / 2
     var blinkOn by liveVarOf(false)
+    var healthPoints by liveVarOf((8..16).random())
+    var cash by liveVarOf(0)
+
+    // val spinnerAnim = textAnimOf(listOf("\\", "|", "/", "-"), Duration.ofMillis(125))
+
+    fun tryMoveHero(tile: Tile, move: () -> Unit) {
+        if (tile is Cash) {
+            cash += 1
+            tile.isTaken = true
+        }
+        if (tile is Lava) healthPoints = (healthPoints - 6).coerceAtLeast(0)
+        if (tile is Water) healthPoints = (healthPoints - 1).coerceAtLeast(0)
+        if (!tile.isOpaque) move()
+    }
+
     section {
         textLine("Move hero with w a s d or Q to quit")
     }.run()
+
     section {
         // section is rendered on each move
 
         for ((indexY, row) in world.withIndex()) {
             for ((indexX, _) in row.withIndex()) {
                 world[indexY][indexX].isVisible = false // hide all tiles - visible radius is based on hero position
+                if (world[indexY][indexX].isTaken) {
+                    world[indexY][indexX] = Floor()
+                }
             }
         }
 
@@ -95,72 +124,81 @@ fun main() = session(
 
         RayCast.renderHeroRadius(xhero, yhero, view, 6)
 
-        repeat(padTop) { textLine() }
-
-        shiftRight(padRight) {
-            cyan {
-                bordered(BorderCharacters.CURVED, paddingLeftRight = 1, paddingTopBottom = 1) {
-                    view.mapIndexed { y, rows ->
-                        rows.mapIndexed { x, tile ->
-                            if (yhero == y && xhero == x) render(Hero, false)
-                            else if (tile.isVisible) render(tile, if (tile is Lava) blinkOn else false)
-                            else text(Empty.glyph)
-                        }
-                        textLine()
+        statusPanels(
+            health = { text("Health: $healthPoints") },
+            cash = { text("Cash: $cash of $TOTAL_CASH") }
+        )
+        cyan {
+            bordered(BorderCharacters.CURVED, paddingLeftRight = 1, paddingTopBottom = 1) {
+                view.mapIndexed { y, rows ->
+                    rows.mapIndexed { x, tile ->
+                        //  if (yhero == y + 1 && xhero == x + 1) { text(spinnerAnim) }
+                        if (yhero == y && xhero == x) render(Hero, false)
+                        else if (tile.isVisible) render(tile, if (tile is Lava) blinkOn else false)
+                        else text(Empty.glyph)
                     }
+                    textLine()
                 }
             }
         }
     }
+        //   }
         .onFinishing { blinkOn = false }
         .runUntilKeyPressed(Keys.Q_UPPER) {
             addTimer(Duration.ofMillis(1000), repeat = true) { blinkOn = !blinkOn }
             onKeyPressed {
                 when (key) {
+
+                    Keys.R_UPPER -> {
+
+                    }
+
                     Keys.W -> {
                         // Can move across Floor tiles only
-                        if (world[yView.min + yhero - 1][xView.min + xhero] !is Floor) return@onKeyPressed
+                        tryMoveHero(world[yView.min + yhero - 1][xView.min + xhero]) {
+                            // range prior to moving hero used to scroll top or bottom position
+                            val yMinPrev = yView.min
 
-                        // range prior to moving hero used to scroll top or bottom position
-                        val yMinPrev = yView.min
+                            // the hero is "fixed" to middle of the view unless near the edges where the view is "fixed"
+                            yView = if (yhero == yMaxIndex / 4) decView(yView, 1, 0) else yView
 
-                        // the hero is "fixed" to middle of the view unless near the edges where the view is "fixed"
-                        yView = if (yhero == yMaxIndex / 4) decView(yView, 1, 0) else yView
+                            // the hero is allowed to move to the edges when the view is scrolled to max range
+                            yhero =
+                                (if (yView.max == yMaxIndex || yMinPrev == 0) (yhero - 1).coerceAtLeast(0) else yhero)
+                        }
 
-                        // the hero is allowed to move to the edges when the view is scrolled to max range
-                        yhero = (if (yView.max == yMaxIndex || yMinPrev == 0) (yhero - 1).coerceAtLeast(0) else yhero)
                     }
 
                     Keys.S -> {
-                        if (world[yView.min + yhero + 1][xView.min + xhero] !is Floor) return@onKeyPressed
+                        tryMoveHero(world[yView.min + yhero + 1][xView.min + xhero]) {
+                            val yPrev = yView.max
 
-                        val yPrev = yView.max
+                            yView = if (yhero == yMaxIndex / 4) incView(yView, 1, yMaxIndex) else yView
 
-                        yView = if (yhero == yMaxIndex / 4) incView(yView, 1, yMaxIndex) else yView
-
-                        yhero =
-                            (if (yView.min == 0 || yPrev == yMaxIndex) (yhero + 1).coerceAtMost(yMaxIndex) else yhero)
+                            yhero =
+                                (if (yView.min == 0 || yPrev == yMaxIndex) (yhero + 1).coerceAtMost(yMaxIndex) else yhero)
+                        }
                     }
 
                     Keys.A -> {
-                        if (world[yView.min + yhero][xView.min + xhero - 1] !is Floor) return@onKeyPressed
+                        tryMoveHero(world[yView.min + yhero][xView.min + xhero - 1]) {
+                            val xPrev = xView.min
 
-                        val xPrev = xView.min
+                            xView = if (xhero == xMaxIndex / 4) decView(xView, 1, 0) else xView
 
-                        xView = if (xhero == xMaxIndex / 4) decView(xView, 1, 0) else xView
-
-                        xhero = (if (xView.max == xMaxIndex || xPrev == 0) (xhero - 1).coerceAtLeast(0) else xhero)
+                            xhero = (if (xView.max == xMaxIndex || xPrev == 0) (xhero - 1).coerceAtLeast(0) else xhero)
+                        }
                     }
 
                     Keys.D -> {
-                        if (world[yView.min + yhero][xView.min + xhero + 1] !is Floor) return@onKeyPressed
+                        tryMoveHero(world[yView.min + yhero][xView.min + xhero + 1]) {
+                            val xPrev = xView.max
 
-                        val xPrev = xView.max
+                            xView = if (xhero == xMaxIndex / 4) incView(xView, 1, xMaxIndex) else xView
 
-                        xView = if (xhero == xMaxIndex / 4) incView(xView, 1, xMaxIndex) else xView
-
-                        xhero =
-                            (if (xView.min == 0 || xPrev == xMaxIndex) (xhero + 1).coerceAtMost(xMaxIndex) else xhero)
+                            xhero =
+                                (if (xView.min == 0 || xPrev == xMaxIndex) (xhero + 1).coerceAtMost(xMaxIndex) else xhero)
+                        }
                     }
                 }
             }
